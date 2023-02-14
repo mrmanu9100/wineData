52304b379cd3bb9fabdf75241a9ade0c93cf0db5
mrmanu9100 <manojmargam830@gmail.com>
2023-02-14 19:38:10 +0530

    code changes for Showing 3 Bars only

--- src/BarChart.js
+++ src/BarChart.js
@@ -4,17 +4,21 @@ import Data from './Data.json'
 
 //This component is for Bar Chart
 const BarChart = () => {
+  const xAxisData = Data.wineData.map((data)=>data.Alcohol)
+  //Here im filtering the Alcohol data and removing duplicates
+  const alcoholData=[...new Set(xAxisData)]
+  console.log()
   const option = {
     title: {
       text: 'Wine Data'
     },
     tooltip: {},
     legend: {
-      data:[''] //Did nt taking legend data
+      data:[''] //Did n't taking legend data
     },
     xAxis: {
       //Here I am mapping the Alcohol Data from Json response
-      data:Data.wineData.map((data)=>data.Alcohol)
+      data:alcoholData
     },
     yAxis: {},
     series: [{
